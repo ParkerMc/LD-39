58b81a1f21c992529a67501b2bc60fce17d064ba
ParkerMc <k5npr@yahoo.com>
2017-07-29 02:02:09 -0500

    Finished player, need to add 3D colliders

--- Assets/scripts/Player_look.cs
+++ Assets/scripts/Player_look.cs
@@ -3,18 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player_look : MonoBehaviour {
-	public float sensitivityX;
-	public float sensitivityY;
+	public float sensitivity = 20;
 
-	void Update () {
+	void Start(){
+		Rigidbody rb = this.GetComponent<Rigidbody> ();
+		rb.freezeRotation = true;
+	}
+
+	void FixedUpdate () {
 		if(Input.GetKeyDown(KeyCode.Escape)) {
 			Cursor.lockState = CursorLockMode.None;
 		}else if (Input.GetMouseButtonDown (0)) {
 			Cursor.lockState = CursorLockMode.Locked;
 		}
 		if (Cursor.lockState == CursorLockMode.Locked) {
-			float rotX = Input.GetAxis ("Mouse X") * sensitivityX;
-			float rotY = Input.GetAxis ("Mouse Y") * sensitivityY;
+			float rotX = Input.GetAxis ("Mouse X") * sensitivity * Time.deltaTime;
+			float rotY = Input.GetAxis ("Mouse Y") * sensitivity * Time.deltaTime;
 			transform.localRotation = Quaternion.Euler (transform.localRotation.eulerAngles.x - rotY, transform.localRotation.eulerAngles.y + rotX, 0);
 		}
 	}
---
+++ Assets/scripts/Player_move.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_move : MonoBehaviour {
+	public float sensitivity = 50f;
+	public float jumpSpeed = 60f;
+	private float disToGround;
+
+	void Start(){
+		disToGround = this.GetComponent<Collider> ().bounds.extents.y;
+	}
+
+	void FixedUpdate () {
+		//Move along x and z
+		float Xaxes =  Input.GetAxis("Horizontal");
+		float Zaxes =  Input.GetAxis("Vertical");
+		Rigidbody rb = this.GetComponent<Rigidbody>();
+		float speed = 1f;
+		if (Input.GetButton ("Fire3")) {
+			speed = 2f;
+		}
+		rb.velocity = new Vector3 (0, rb.velocity.y, 0) + ( Zaxes * speed * new Vector3 (transform.forward.x,0,transform.forward.z)) + ( Xaxes * speed * new Vector3 (transform.right.x,0,transform.right.z)) * Time.deltaTime * sensitivity;
+		//Jump
+		if (Input.GetButton ("Jump") && Physics.Raycast (transform.position, -Vector3.up, disToGround + 0.1f)) {
+			rb.AddForce (Vector3.up * jumpSpeed);
+		}
+	}
+}
