5fc26b10a37d37c848924ad61049c0ea9a121e34
ParkerMc <k5npr@yahoo.com>
2017-07-29 00:49:17 -0500

    Added Player look files

---
+++ Assets/scripts/Player_look.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_look : MonoBehaviour {
+	public float sensitivityX;
+	public float sensitivityY;
+
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Escape)) {
+			Cursor.lockState = CursorLockMode.None;
+		}else if (Input.GetMouseButtonDown (0)) {
+			Cursor.lockState = CursorLockMode.Locked;
+		}
+		if (Cursor.lockState == CursorLockMode.Locked) {
+			float rotX = Input.GetAxis ("Mouse X") * sensitivityX;
+			float rotY = Input.GetAxis ("Mouse Y") * sensitivityY;
+			transform.localRotation = Quaternion.Euler (transform.localRotation.eulerAngles.x - rotY, transform.localRotation.eulerAngles.y + rotX, 0);
+		}
+	}
+}
