29daa6b9b5ff7d37c77f7f4614a51d3a835894df
ParkerMc <k5npr@yahoo.com>
2017-07-30 14:53:59 -0500

    Fixed yellow dot parenting and made light togaleable

---
+++ Assets/Scripts/Power.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Power : MonoBehaviour {
+	private bool flashlightOn = false;
+	public Light flashlight;
+
+	void Start() {
+		flashlight.enabled = false;
+	}
+
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Q)) {
+			if (flashlightOn) {
+				flashlight.enabled = false;
+				flashlightOn = false;
+			} else {
+				flashlight.enabled = true;
+				flashlightOn = true;
+			}
+		}
+	}
+}
