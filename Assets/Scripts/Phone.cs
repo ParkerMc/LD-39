bc3b6e78e5e5f91b581af58b5981b2b41718c565
ParkerMc <k5npr@yahoo.com>
2017-07-31 03:57:21 -0500

    More work on phone

--- Assets/Scripts/Phone.cs
+++ Assets/Scripts/Phone.cs
@@ -2,23 +2,94 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//todo after animation change map code
 public class Phone : MonoBehaviour {
-	public GameObject pannel;
-	public GameObject lockText;
-	public GameObject screenLight;
+	public GameObject home;
+	public GameObject topBar;
+	public GameObject flashlight;
 	public GameObject map;
+	public GameObject flashlightL;
+	public UnityEngine.UI.Text powerLevel;
+	public float lightPower;
+	public float mapPower;
+	private int powLev;
+	private bool mapUp = false;
+	private bool flashlightOn = false;
+	private bool screenOn = false;
+	private Animator animator;
 
-	private bool animationDone = true;
-	void Start () {
-		pannel.SetActive (true);
-		lockText.SetActive (true);
-		screenLight.SetActive (true);
+	void Start(){
+		animator = this.GetComponentInParent<Animator> ();
 	}
 	
 
 	void Update () {
-		if (Input.GetKeyDown (KeyCode.M) && animationDone) {
-			
+		if (animator.GetCurrentAnimatorStateInfo (0).IsName ("armUp") && !screenOn) {
+			topBar.SetActive (true);
+			home.SetActive (true);
+			screenOn = true;
+		} else if (animator.GetCurrentAnimatorStateInfo (0).IsName ("TurningPhoneOff")&&screenOn&&animator.GetCurrentAnimatorStateInfo(0).normalizedTime > .275) {
+			topBar.SetActive (false);
+			home.SetActive (false);
+			screenOn = false;
+		} else if(animator.GetCurrentAnimatorStateInfo (0).IsName("TurningFlashlightOn")&&animator.GetCurrentAnimatorStateInfo (0).normalizedTime > .426&&!flashlightOn){
+			home.SetActive (false);
+			flashlight.SetActive (true);
+			flashlightL.SetActive (true);
+			flashlightOn = true;
+		}else if(animator.GetCurrentAnimatorStateInfo (0).IsName("TurningFlashlightOff")&&animator.GetCurrentAnimatorStateInfo (0).normalizedTime > .492&&flashlightOn){
+			home.SetActive (true);
+			flashlight.SetActive (false);
+			flashlightL.SetActive (false);
+			flashlightOn = false;
+		}
+		if (Input.GetKeyDown (KeyCode.M)&&Power.HasPower()) {
+			if (!mapUp) {
+				animator.SetBool ("armUp", true);
+				animator.SetBool ("flashlight", false);
+				home.SetActive (false);
+				map.SetActive (true);
+				mapUp = true;
+			} else {
+				animator.SetBool ("armUp", false);
+				home.SetActive (true);
+				map.SetActive (false);
+				mapUp = false;
+			}
+		} else if (Input.GetKeyDown (KeyCode.F)&&Power.HasPower()) {
+			if (!flashlightOn) {
+				map.SetActive (false);
+				mapUp = false;
+				animator.SetBool ("armUp", true);
+				animator.SetBool ("flashlight", true);
+			} else {
+				animator.SetBool ("armUp", false);
+				animator.SetBool ("flashlight", false);
+			}
+		}
+
+		if (flashlightOn) {
+			Power.TakePower (lightPower * Time.deltaTime);
+		}else if (mapUp) {
+			Power.TakePower (mapPower * Time.deltaTime);
+		}
+		if (!Power.HasPower()) {
+			flashlight.SetActive(false);
+			topBar.SetActive (false);
+			home.SetActive (false);
+			map.SetActive (false);
+			flashlightL.SetActive (false);
+			flashlightOn = false;
+			mapUp = false;
+			animator.SetBool ("power", false);
+		}
+		powLev = Power.PowerLevel ();
+		if (powLev >= 37) {
+			powerLevel.text = "\\uf242 " + powLev.ToString () + "%";
+		}else if (powLev >= 12) {
+			powerLevel.text = "\\uf243 " + powLev.ToString () + "%";
+		}else {
+			powerLevel.text = "\\uf244 " + powLev.ToString () + "%";
 		}
 	}
 }
--- Assets/Scripts/Power.cs
+++ Assets/Scripts/Power.cs
@@ -3,18 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Power : MonoBehaviour {
-	private bool flashlightOn = false;
-	private Animator animator;
-	private bool animationDone = true;
 
-	private static int power = 20;
+	private static int power = 50;
 	private static float powfloat = 1;
 
 	public static bool pause = false;
-
-	public Light flashlight;
-	public UnityEngine.UI.Text text;
-	public float lightPower;
 
 	public static void TakePower(float amount){
 		if (!pause) {
@@ -35,44 +28,5 @@ public class Power : MonoBehaviour {
 
 	public static int PowerLevel(){
 		return power;
-	}
-	
-	void Start() {
-		flashlight.enabled = false;
-		animator = this.GetComponentInChildren<Animator> ();
-	}
-
-	void Update () {
-		if (Input.GetKeyDown (KeyCode.F)&&animationDone) {
-			if (flashlightOn) {
-				animationDone = false;
-				flashlightOn = false;
-				animator.SetBool ("flashlight", false);
-				animator.SetBool ("armUp", false);
-			} else {
-				animationDone = false;
-				flashlightOn = true;
-				animator.SetBool ("flashlight", true);
-				animator.SetBool ("armUp", true);
-			}
-		}
-		if (!animationDone) {
-			if(animator.GetCurrentAnimatorStateInfo (0).IsName("TurningFlashlightOn")&&animator.GetCurrentAnimatorStateInfo (0).normalizedTime > .426){
-				flashlight.enabled = true;
-				animationDone = true;
-			}else if(animator.GetCurrentAnimatorStateInfo (0).IsName("TurningFlashlightOff")&&animator.GetCurrentAnimatorStateInfo (0).normalizedTime > .492){
-				flashlight.enabled = false;
-				animationDone = true;
-			}
-		}else if (flashlightOn) {
-			TakePower (lightPower*Time.deltaTime);
-			if (!HasPower()) {
-				flashlight.enabled = false;
-				flashlightOn = false;
-				//Todo make power off at once
-				animator.SetBool ("flashlight", false);
-			}
-		}
-		text.text = "Power: " + power.ToString() + "% ";
 	}
 }
